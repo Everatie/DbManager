ba261d0158257f621168da31a058134ae9c61600
Everatie <asd12.alex@tutanota.com>
2023-12-24 01:40:18 -0300

    Main interface
    Search and write interface

---
+++ interface.py
@@ -0,0 +1,156 @@
+from PyInquirer import prompt
+import search
+import json
+import os
+
+class Interface:
+    def __init__(self) -> None:
+        self.format_choice = {
+            'type' : 'list',
+            'name': 'user_option',
+            'message': 'What entry do you want?',
+            'choices': ['Anime', 'Manga', 'Movie', 'Cancel']
+        }
+        self.entry_choice = {
+            'type' : 'list',
+            'name': 'user_option',
+            'message': 'What entry do you want?',
+            'choices': []
+        }
+        self.format = False
+    
+    def AddEntry(self):
+        media_data = self.FormatChoice() #chooses the format
+        
+        # Check for errors in request
+        if isinstance(media_data, str):
+            print(media_data)
+            return False
+
+        desired_entry = self.EntryChoice(media_data)
+        
+        # Check if user opted for 'None'
+        if desired_entry == None:
+            return False
+
+        filtered_data = self.FilterData(media_data, desired_entry)
+        
+        if self.format == "ANIME": 
+            self.DbWrite(filtered_data, r'.\Databases\Anime.json')
+
+        elif self.format == "MANGA": 
+            self.DbWrite(filtered_data, r'.\Databases\Manga.json')
+
+        elif self.format == "MOVIE": 
+            self.DbWrite(filtered_data, r'.\Databases\Movie.json')
+
+    def FormatChoice(self) -> list:
+        format = prompt(self.format_choice)
+
+        if format.get("user_option") == "Anime":
+            self.format = "ANIME"
+
+            anime_name = input('Please input the name of the desired anime: ').strip()
+            return search.AnilistData(anime_name, self.format)["data"]["Page"]["media"] #removes  junk
+
+        elif format.get("user_option") == "Manga":
+            self.format = "MANGA"
+
+            manga_name = input('Please input the name of the desired anime: ').strip()
+            return search.AnilistData(manga_name, self.format)["data"]["Page"]["media"] #removes  junk
+
+        elif format.get("user_option") == "Movie": 
+            self.format = "MOVIE"
+
+            movie_name = input('Please input the name of the desired movie: ').strip()
+            return search.MovieSearch(movie_name)['results']
+
+        elif format.get("user_option") == "Cancel":
+            return 'Operation cancelled'
+
+    def EntryChoice(self, entries:list) -> int: 
+        # Texts for each type of midia
+        if self.format == 'ANIME' or self.format == 'MANGA':
+            for entry in entries:
+                entry_text = '{} ({}) -- {}\n'.format(
+                    entry['title']['english'], entry['title']['romaji'],
+                    entry['startDate']['year'])
+                self.entry_choice['choices'].append(entry_text)            
+        
+        elif self.format == 'MOVIE':
+            for entry in entries:
+                entry_text = '{} -- {}\n'.format(entry['original_title'], entry['release_date'])
+                self.entry_choice['choices'].append(entry_text)
+        
+        self.entry_choice['choices'].append('None')
+        
+        #Visual stuff
+        desired_entry = prompt(self.entry_choice)
+        
+        if desired_entry['user_option'] == 'None':
+            return None
+        
+        return self.entry_choice['choices'].index(desired_entry['user_option'])
+    
+    def FilterData(self, entries:list, choosen_index:int) -> dict:
+        filtered_entry = entries[choosen_index]
+
+        if self.format == 'ANIME':
+            episodes = input('How many episodes have you watched? ')
+            filtered_entry['episode'] = episodes
+        
+        elif self.format == 'MANGA':
+            chapters = input('How many chapters have you read? ')
+            filtered_entry['chapters'] = chapters
+        
+        elif self.format == 'MOVIE':
+            #get details omitted in the previous dict
+            id = filtered_entry['id']
+            unfiltered_entry = search.MovieData(id)
+
+            filtered_entry = {} # Refresh the filtered
+
+            # Add the relevant things
+            filtered_entry['genre'] = []
+            for i in range(len(unfiltered_entry['genres'])):
+                filtered_entry['genre'].append(unfiltered_entry['genres'][i]['name'])
+
+            filtered_entry['countryOfOrigin'] = []
+            for i in range(len(unfiltered_entry['production_countries'])):
+                filtered_entry['countryOfOrigin'].append(unfiltered_entry['production_countries'][i]['name'])
+
+            filtered_entry['title']= {}
+            filtered_entry['title']['original'] = unfiltered_entry['original_title']
+            filtered_entry['title']['brazilian'] = input('Insira o título em português: ')
+            filtered_entry['release_date'] = unfiltered_entry['release_date']
+            filtered_entry['runtime'] = unfiltered_entry['runtime']
+        
+        filtered_entry['thoughts'] = input('Any final thoughts?\n')
+        return filtered_entry
+            
+    def DbWrite(self, entry, target_db) -> json:
+        if os.path.exists(target_db):
+            with open(target_db, 'r') as file:
+                data = file.read()
+
+            if os.stat(target_db).st_size != 0:
+                # Remove the trailing ']'
+                data = data.rstrip(']')
+                with open(target_db, 'w') as file:
+                    file.write(data)
+                    file.write(',\n')
+                    json.dump(entry, file, indent=2, ensure_ascii=False)
+                    file.write(']')
+            else:
+                with open(target_db, 'w') as file:
+                    file.write('[')
+                    json.dump(entry, file, indent=2, ensure_ascii=False)
+                    file.write(']')
+        else:
+            with open(target_db, 'w') as file:
+                file.write('[')
+                json.dump(entry, file, indent=2, ensure_ascii=False)
+                file.write(']')
+                
+Interface = Interface()
+Interface.AddEntry()
